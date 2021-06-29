de2196a38147b247f9c996fccfb49916124b92ca
ayush-rathore <aayushh79@gmail.com>
2021-06-29 23:10:41 +0530

    Add News Models

---
+++ back-end/models/general.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const News = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = mongoose.model("general", News, "general");
