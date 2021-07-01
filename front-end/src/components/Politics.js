9b37a2cc5358a240e3876b1cacecf64fc3bdbbbb
ayush-rathore <aayushh79@gmail.com>
2021-07-01 22:52:58 +0530

    Add Routes for News

---
+++ front-end/src/components/Politics.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import "../css/general.css";
+
+const Politics = () => {
+    const [news, setNews] = useState([]);
+    const fetchNews = async () => {
+        await Axios.get("http://localhost:8080/politics")
+            .then(({ data }) => {
+                console.info(data);
+                setNews(data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+    useEffect(() => {
+        fetchNews();
+    }, []);
+    const renderNews = news.map((article, index) => {
+        return (
+            <div id="scontainer1-news" key={index}>
+                <img src={article.image} alt={"news"} /> <br />
+                <p>{article.description}</p>
+            </div>
+        );
+    });
+    const article = news[0];
+    console.log(article);
+    return (
+        <>
+            <h1 id="sh1">Politics</h1>
+            <br />
+            <div id="scontainer">
+                <div id="smaincard">
+                    {/* <img src={article.image} alt="close" />
+                    <p>
+                        {article.title}
+                        <br />
+                    </p>
+                    <a href={news.url}>Read More</a> */}
+                </div>
+                <div id="sheadlines">
+                    <h2 id="sh2">Headlines</h2>
+                    <div id="sheadlines-news">
+                        <p>
+                            What is Lorem Ipsum?Where does it come from?
+                            <br />
+                            Lorem Ipsum is simply dummy text of the typesetting
+                            industry.
+                        </p>
+                    </div>
+                    <div id="sheadlines-news">
+                        <p>
+                            What is Lorem Ipsum?Where does it come from?
+                            <br />
+                            Lorem Ipsum is simply dummy text of the typesetting
+                            industry.
+                        </p>
+                    </div>
+                    <div id="sheadlines-news">
+                        <p>
+                            What is Lorem Ipsum?Where does it come from?
+                            <br />
+                            Lorem Ipsum is simply dummy text of the typesetting
+                            industry.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div id="scontainer1">{renderNews}</div>
+        </>
+    );
+};
+
+export default Politics;
