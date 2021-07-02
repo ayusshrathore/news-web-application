7f4422a61852d8ffe5b9161b752c3a323baa29f6
Rohit-Rajpurohit <rajpurohitrohit869@gmail.com>
2021-07-02 23:13:38 +0530

    Add href for news

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../css/home.css";
-import body from "./body.png";
+import body from "./images/body.png";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/swiper.min.css";
 import "swiper/components/effect-fade/effect-fade.min.css";
@@ -10,55 +10,59 @@ import Axios from "axios";
 SwiperCore.use([EffectFade, Autoplay]);
 
 const Home = () => {
-    const [news, setNews] = useState([]);
-    const fetchNews = async () => {
-        await Axios.get("http://localhost:8080/general")
-            .then(({ data }) => {
-                console.info(data);
-                setNews(data);
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    };
-    useEffect(() => {
-        fetchNews();
-    }, []);
-    const renderNews = news.map((article, index) => {
-        return (
-            <SwiperSlide key={index}>
-                <img src={article.image} alt={"news"} />
-                <div id={"title"}>{article.title}</div>
-            </SwiperSlide>
-        );
-    });
+  const [news, setNews] = useState([]);
+  const fetchNews = async () => {
+    await Axios.get("http://localhost:8080/general")
+      .then(({ data }) => {
+        console.info(data);
+        setNews(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  useEffect(() => {
+    fetchNews();
+  }, []);
+  const renderNews = news.map((article, index) => {
     return (
-        <div>
-            <div className="container">
-                <div id="item1">
-                    <img src={body} alt={"alt"} />
-                </div>
-                <div id="item2">
-                    <h1>DISCOVER...</h1>
-                    <p>
-                        Take some time and discover <br /> what's happening
-                        around the globe.
-                    </p>
-                    <div className={"carousel"}>
-                        <Swiper
-                            spaceBetween={30}
-                            loop={true}
-                            effect={"fade"}
-                            autoplay={{ delay: 3000 }}
-                            className="mySwiper"
-                        >
-                            {renderNews}
-                        </Swiper>
-                    </div>
-                </div>
-            </div>
+      <SwiperSlide key={index}>
+        <img src={article.image} alt={"news"} />
+        <div id={"title"}>
+          {article.title}
+          <br /> <br />
+          <a href={article.url}>Read More</a>
         </div>
+      </SwiperSlide>
     );
+  });
+  return (
+    <>
+      <div className="container">
+        <div id="item1">
+          <img src={body} alt={"alt"} />
+        </div>
+        <div id="item2">
+          <h1>DISCOVER...</h1>
+          <p>
+            Take some time and discover <br /> what's happening around the
+            globe.
+          </p>
+          <div className="carousel">
+            <Swiper
+              spaceBetween={30}
+              loop={true}
+              effect={"fade"}
+              autoplay={{ delay: 3000 }}
+              className="mySwiper"
+            >
+              {renderNews}
+            </Swiper>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
