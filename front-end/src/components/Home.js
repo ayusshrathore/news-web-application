9a523f83a1ece7f951783b2a55de457c7a9e010b
ayush-rathore <aayushh79@gmail.com>
2021-07-03 09:14:30 +0530

    Fetch news via API

--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -10,59 +10,61 @@ import Axios from "axios";
 SwiperCore.use([EffectFade, Autoplay]);
 
 const Home = () => {
-  const [news, setNews] = useState([]);
-  const fetchNews = async () => {
-    await Axios.get("http://localhost:8080/general")
-      .then(({ data }) => {
-        console.info(data);
-        setNews(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-  useEffect(() => {
-    fetchNews();
-  }, []);
-  const renderNews = news.map((article, index) => {
+    const [news, setNews] = useState([]);
+    const url =
+        "https://api.currentsapi.services/v1/latest-news?apiKey=2pTytc0fCfwtIH5e7F16sYiP70MRteZHswUtjaW4i5uMU9Gm&language=en&country=in&category=general";
+    const fetchNews = async () => {
+        await Axios.get(url)
+            .then((response) => {
+                console.info(response.data.news);
+                setNews(response.data.news);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+    useEffect(() => {
+        fetchNews();
+    }, []);
+    const renderNews = news.map((article, index) => {
+        return (
+            <SwiperSlide key={index}>
+                <img src={article.image} alt={"news"} />
+                <div id={"title"}>
+                    {article.title}
+                    <br /> <br />
+                    <a href={article.url}>Read More</a>
+                </div>
+            </SwiperSlide>
+        );
+    });
     return (
-      <SwiperSlide key={index}>
-        <img src={article.image} alt={"news"} />
-        <div id={"title"}>
-          {article.title}
-          <br /> <br />
-          <a href={article.url}>Read More</a>
-        </div>
-      </SwiperSlide>
+        <>
+            <div className="container">
+                <div id="item1">
+                    <img src={body} alt={"alt"} />
+                </div>
+                <div id="item2">
+                    <h1>DISCOVER...</h1>
+                    <p>
+                        Take some time and discover <br /> what's happening
+                        around the globe.
+                    </p>
+                    <div className="carousel">
+                        <Swiper
+                            spaceBetween={30}
+                            loop={true}
+                            effect={"fade"}
+                            autoplay={{ delay: 3000 }}
+                            className="mySwiper"
+                        >
+                            {renderNews}
+                        </Swiper>
+                    </div>
+                </div>
+            </div>
+        </>
     );
-  });
-  return (
-    <>
-      <div className="container">
-        <div id="item1">
-          <img src={body} alt={"alt"} />
-        </div>
-        <div id="item2">
-          <h1>DISCOVER...</h1>
-          <p>
-            Take some time and discover <br /> what's happening around the
-            globe.
-          </p>
-          <div className="carousel">
-            <Swiper
-              spaceBetween={30}
-              loop={true}
-              effect={"fade"}
-              autoplay={{ delay: 3000 }}
-              className="mySwiper"
-            >
-              {renderNews}
-            </Swiper>
-          </div>
-        </div>
-      </div>
-    </>
-  );
 };
 
 export default Home;
--- front-end/src/components/Sports.js
+++ front-end/src/components/Sports.js
@@ -3,69 +3,70 @@ import Axios from "axios";
 import "../css/general.css";
 
 const Sports = () => {
-  const [news, setNews] = useState([]);
-  const fetchNews = async () => {
-    await Axios.get("http://localhost:8080/sports")
-      .then(({ data }) => {
-        console.info(data);
-        setNews(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
-  useEffect(() => {
-    fetchNews();
-  }, []);
-  const renderNews = news.map((article, index) => {
+    const [news, setNews] = useState([]);
+    const url =
+        "https://api.currentsapi.services/v1/latest-news?apiKey=2pTytc0fCfwtIH5e7F16sYiP70MRteZHswUtjaW4i5uMU9Gm&language=en&country=in&category=sports";
+    const fetchNews = async () => {
+        await Axios.get(url)
+            .then((response) => {
+                console.info(response.data.news);
+                setNews(response.data.news);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
+    useEffect(() => {
+        fetchNews();
+    }, []);
+    const renderNews = news.map((article, index) => {
+        return (
+            <div id="scontainer1-news" key={index}>
+                <img src={article.image} alt={"news"} /> <br />
+                <p>{article.title}</p>
+            </div>
+        );
+    });
+    const main = news.filter((article, index) => {
+        return index === 0;
+    });
+    const renderMain = main.map((article, index) => {
+        return (
+            <div id="smaincard" key={index}>
+                <img src={article.image} alt="close" />
+                <p>
+                    {article.title}
+                    <br />
+                </p>
+                {/* <a href={article.url}>Read More</a> */}
+            </div>
+        );
+    });
+    const headlines = news.filter((article, index) => {
+        return index <= 2;
+    });
+    console.log(headlines);
+    const renderHeadlines = headlines.map((article, index) => {
+        return (
+            <div id="sheadlines-news" key={index}>
+                <p> {article.title}</p>
+            </div>
+        );
+    });
     return (
-      <div id="scontainer1-news" key={index}>
-        <img src={article.image} alt={"news"} /> <br />
-        <p>{article.title}</p>
-      </div>
+        <>
+            <h1 id="sh1">Sports</h1>
+            <br />
+            <div id="scontainer">
+                {renderMain}
+                <div id="sheadlines">
+                    <h2 id="sh2">Headlines</h2>
+                    {renderHeadlines}
+                </div>
+            </div>
+            <div id="scontainer1">{renderNews}</div>
+        </>
     );
-  });
-  const main = news.filter((article, index) => {
-    return index === 0;
-  });
-  const renderMain = main.map((article, index) => {
-    return (
-      <div id="smaincard" key={index}>
-        <img src={article.image} alt="close" />
-        <p>
-          {article.title}
-          <br />
-        </p>
-        <a href={article.url}>Read More</a>
-      </div>
-    );
-  });
-  const headlines = news.filter((article, index) => {
-    return index <= 2;
-  });
-  console.log(headlines);
-  const renderHeadlines = headlines.map((article, index) => {
-    return (
-      <div id="sheadlines-news" key={index}>
-        <p> {article.title}</p>
-      </div>
-    );
-  });
-  return (
-    <>
-      <h1 id="sh1">Sports</h1>
-      <br />
-      <div id="scontainer">
-        {renderMain}
-        <div id="sheadlines">
-          <h2 id="sh2">Headlines</h2>
-          {renderHeadlines}
-          <a>Read more </a>
-        </div>
-      </div>
-      <div id="scontainer1">{renderNews}</div>
-    </>
-  );
 };
 
 export default Sports;
