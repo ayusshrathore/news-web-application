eb96f08de9de6e689963c2820ee68ff484a9dd6e
Rohit-Rajpurohit <rajpurohitrohit869@gmail.com>
2021-07-22 01:01:41 +0530

    Improve Hamburger Menu

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,37 +4,37 @@ import body from "./images/news.jpg";
 import { useHistory } from "react-router";
 
 const Home = () => {
-    const history = useHistory();
-    const refresh = () => {
-        window.location.reload(false);
-    };
-    return (
-        <>
-            <div className="container">
-                <div id="item2">
-                    <h1>
-                        What's the <br /> News Today ?
-                    </h1>
-                    <p>
-                        Take some time and discover <br /> what's happening
-                        around the globe.
-                    </p>
-                    <div
-                        id="btn"
-                        onClick={() => {
-                            history.push("/general");
-                            refresh();
-                        }}
-                    >
-                        Read More &nbsp;&nbsp;<span>&#8594;</span>
-                    </div>
-                </div>
-                <div id="item1">
-                    <img src={body} alt={"body"} />
-                </div>
-            </div>
-        </>
-    );
+  const history = useHistory();
+  const refresh = () => {
+    window.location.reload(false);
+  };
+  return (
+    <>
+      <div className="container">
+        <div id="item2">
+          <h1>
+            What's the <br /> News Today ?
+          </h1>
+          <p>
+            Take some time and discover <br /> what's happening around the
+            globe.
+          </p>
+          <div
+            id="btn"
+            onClick={() => {
+              history.push("/general");
+              refresh();
+            }}
+          >
+            Read More &nbsp;&nbsp;<span>&#8594;</span>
+          </div>
+        </div>
+        <div id="item1">
+          <img src={body} alt={"body"} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
