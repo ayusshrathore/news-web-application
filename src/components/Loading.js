9093b56e87b5081716863cc68d03d1c52ab314ae
ayush-rathore <aayushh79@gmail.com>
2022-01-01 23:29:40 +0530

    Add loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "../css/loading.css";
+import loader from "./images/loader.svg";
 
 const Loading = ({ visible }) => {
 	if (!visible) return null;
 
 	return (
 		<div className="loading">
-			<img id="loader" src="./images/loader.svg" alt="loader"></img>
+			<img id="loader" src={loader} alt="loader"></img>
 		</div>
 	);
 };
