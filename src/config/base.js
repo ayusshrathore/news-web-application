a7832b194b834014b50d63b1b3cacdb38fd626a3
ayush-rathore <aayushh79@gmail.com>
2022-01-01 22:10:40 +0530

    Use env variables for routes

---
+++ src/config/base.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+
+export const GENERAL = process.env.REACT_APP_GENERAL;
+export const BUSINESS = process.env.REACT_APP_BUSINESS;
+export const POLITICS = process.env.REACT_APP_POLITICS;
+export const SPORTS = process.env.REACT_APP_SPORTS;
