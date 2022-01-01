dd307540d88b7abaa145d9075c53288cba78bf7e
ayush-rathore <aayushh79@gmail.com>
2022-01-01 22:09:40 +0530

    Refactor code

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+require("dotenv").config();
 
 ReactDOM.render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById("root")
+	<React.StrictMode>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
