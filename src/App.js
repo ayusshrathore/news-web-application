a9d992acd7f992f0e8b11f1cba7a4555330a4fe1
Rohit-Rajpurohit <rajpurohitrohit869@gmail.com>
2022-01-18 18:25:45 +0530

    refactor code
    extract separate components for footer and navbar

--- src/App.js
+++ src/App.js
@@ -1,139 +1,28 @@
 import React from "react";
-import {
-	BrowserRouter as Router,
-	Route,
-	Switch,
-	Redirect,
-	useHistory,
-} from "react-router-dom";
-
+import { Route, Switch, Redirect } from "react-router-dom";
 import Home from "./components/Home";
 import General from "./components/General";
 import Politics from "./components/Politics";
 import Business from "./components/Business";
 import Sports from "./components/Sports";
-import { useState } from "react";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
 
 const App = () => {
-	const history = useHistory();
-	const refresh = () => {
-		window.location.reload(false);
-	};
-	const [isOpen, setIsOpen] = useState(false);
-
-	return (
-		<>
-			<div id="header">
-				<div className="navbar">
-					<div
-						className="nav-logo"
-						onClick={() => {
-							history.push("/");
-							refresh();
-						}}
-					>
-						The Morning Tribute
-					</div>
-					<ul
-						className={`${isOpen ? "nav-menu-active" : "nav-menu"}`}
-					>
-						<li className="nav-item">
-							<div
-								className="nav-link"
-								onClick={() => {
-									history.push("/general");
-									refresh();
-								}}
-							>
-								General
-							</div>
-						</li>
-						<li className="nav-item">
-							<div
-								className="nav-link"
-								onClick={() => {
-									history.push("/politics");
-									refresh();
-								}}
-							>
-								Politics
-							</div>
-						</li>
-						<li className="nav-item">
-							<div
-								className="nav-link"
-								onClick={() => {
-									history.push("/sports");
-									refresh();
-								}}
-							>
-								Sports
-							</div>
-						</li>
-						<li className="nav-item">
-							<div
-								className="nav-link"
-								onClick={() => {
-									history.push("/business");
-									refresh();
-								}}
-							>
-								Business
-							</div>
-						</li>
-					</ul>
-					<div
-						className={`${
-							isOpen ? "hamburger-active" : "hamburger"
-						}`}
-						onClick={() => {
-							setIsOpen(!isOpen);
-						}}
-					>
-						<span className="bar"></span>
-						<span className="bar"></span>
-						<span className="bar"></span>
-					</div>
-				</div>
-			</div>
-			<>
-				<Router>
-					<Switch>
-						<Route path={"/general"} exact>
-							<General />
-						</Route>
-						<Route path={"/politics"} exact>
-							<Politics />
-						</Route>
-						<Route path={"/sports"} exact>
-							<Sports />
-						</Route>
-						<Route path={"/business"} exact>
-							<Business />
-						</Route>
-						<Route path={"/"}>
-							<Home />
-						</Route>
-						<Route path={"/**"}>
-							<Redirect to={"/"} />
-						</Route>
-					</Switch>
-				</Router>
-			</>
-			<div id="footer">
-				<p>
-					View Source on &nbsp;
-					<a
-						href={
-							"https://github.com/ayush-rathore/news-web-application"
-						}
-					>
-						<i className="fab fa-github"></i> Github
-					</a>
-				</p>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <Switch>
+        <Route path={"/general"} component={General} />
+        <Route path={"/politics"} component={Politics} />
+        <Route path={"/sports"} component={Sports} />
+        <Route path={"/business"} component={Business} />
+        <Route path={"/"} component={Home} exact />
+        <Redirect to={"/"} />
+      </Switch>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
