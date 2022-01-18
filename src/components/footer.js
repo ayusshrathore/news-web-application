c6881832940ded7eead6761d2cc1bb39aa3177a6
Rohit-Rajpurohit <rajpurohitrohit869@gmail.com>
2022-01-18 18:31:21 +0530

    Add components
    remove history and refresh function , use Link in place of div

---
+++ src/components/footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div id="footer">
+      <p>
+        View Source on &nbsp;
+        <a href={"https://github.com/ayush-rathore/news-web-application"}>
+          <i className="fab fa-github"></i> Github
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/navbar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div id="header">
+      <div className="navbar">
+        <Link to="/" className="nav-logo">
+          The Morning Tribute
+        </Link>
+        <ul className={`${isOpen ? "nav-menu-active" : "nav-menu"}`}>
+          <li className="nav-item">
+            <Link to="/general" className="nav-link">
+              General
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/politics" className="nav-link">
+              Politics
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/sports" className="nav-link">
+              Sports
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/business" className="nav-link">
+              Business
+            </Link>
+          </li>
+        </ul>
+        <div
+          className={`${isOpen ? "hamburger-active" : "hamburger"}`}
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        >
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
