5e13fec810d4f6a2454aff01510e01c53708057e
Ayush Rathore <aayushh79@gmail.com>
2022-04-03 00:01:46 +0530

    Fix empty navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,57 +1,77 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { useState } from "react";
-import { Link } from "react-router-dom";
 
 const Navbar = () => {
 	const [isOpen, setIsOpen] = useState(false);
+	const history = useHistory();
+	const refresh = () => {
+		window.location.reload(false);
+	};
 
 	return (
 		<div id="header">
 			<div className="navbar">
-				<Link to="/" className="nav-logo">
+				<div
+					className="nav-logo"
+					onClick={() => {
+						history.push("/");
+						refresh();
+					}}
+				>
 					The Morning Tribute
-				</Link>
+				</div>
 				<ul className={`${isOpen ? "nav-menu-active" : "nav-menu"}`}>
 					<li className="nav-item">
-						<Link
-							to="/general"
+						<div
 							className="nav-link"
-							onClick={setIsOpen(!isOpen)}
+							onClick={() => {
+								history.push("/general");
+								refresh();
+							}}
 						>
 							General
-						</Link>
+						</div>
 					</li>
 					<li className="nav-item">
-						<Link
-							to="/politics"
+						<div
 							className="nav-link"
-							onClick={setIsOpen(!isOpen)}
+							onClick={() => {
+								history.push("/politics");
+								refresh();
+							}}
 						>
 							Politics
-						</Link>
+						</div>
 					</li>
 					<li className="nav-item">
-						<Link
-							to="/sports"
+						<div
 							className="nav-link"
-							onClick={setIsOpen(!isOpen)}
+							onClick={() => {
+								history.push("/sports");
+								refresh();
+							}}
 						>
 							Sports
-						</Link>
+						</div>
 					</li>
 					<li className="nav-item">
-						<Link
-							to="/business"
+						<div
 							className="nav-link"
-							onClick={setIsOpen(!isOpen)}
+							onClick={() => {
+								history.push("/business");
+								refresh();
+							}}
 						>
 							Business
-						</Link>
+						</div>
 					</li>
 				</ul>
 				<div
 					className={`${isOpen ? "hamburger-active" : "hamburger"}`}
-					onClick={setIsOpen(!isOpen)}
+					onClick={() => {
+						setIsOpen(!isOpen);
+					}}
 				>
 					<span className="bar"></span>
 					<span className="bar"></span>
